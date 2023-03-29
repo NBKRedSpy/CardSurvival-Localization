34c7eb4d61751ecb6b1e1e0af727ffc4071e308a
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-03-28 21:50:45 -0400

    publish path fix

--- Package.ps1
+++ Package.ps1
@@ -2,4 +2,4 @@ dotnet publish
 # Get the file name from the C# project name
 $ProjectName = "CardSurvival-Localization"
 
-Compress-Archive -Path "C:\src\CardSurvival\CardSurvival-Localization\CardSurvival-Localization\bin\Debug\net6.0\publish\*" -Force -DestinationPath ./$ProjectName.zip
+Compress-Archive -Path "./CardSurvival-Localization/bin/Debug/net6.0/publish/*" -Force -DestinationPath ./$ProjectName.zip
