6baa94325db603c13854637e7fbf48f63269cd55
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-03-29 19:51:15 -0400

    Readme Update Pacakge folder
    Updated Readme to hopefully be a little more clear.
    Fixed Package.ps1 zip archive including the publish folder.

--- Package.ps1
+++ Package.ps1
@@ -2,4 +2,4 @@ dotnet publish -c Release
 # Get the file name from the C# project name
 $ProjectName = "CardSurvival-Localization"
 
-Compress-Archive -Path "./CardSurvival-Localization/bin/Release/net6.0/publish*" -Force -DestinationPath ./$ProjectName.zip
+Compress-Archive -Path "./CardSurvival-Localization/bin/Release/net6.0/publish/*" -Force -DestinationPath ./$ProjectName.zip
