76f653c372da0856c9eeb208b08310f9e926c448
NBKRedSpy <none@example.com>
2024-12-29 11:09:01 -0500

    WIP

--- CardSurvival-LocalizationTests/SimpFileProcessingTests.cs
+++ CardSurvival-LocalizationTests/SimpFileProcessingTests.cs
@@ -1,6 +1,7 @@
 ﻿using CardSurvival_Localization;
 using System;
 using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -12,8 +13,61 @@ namespace CardSurvival_LocalizationTests
     {
 
         [Fact]
-        public void SimpEn_HasEntry_Success()
+        public void SimpEn_HasFile_ReturnsEntries()
         {
+            MockFileSystem fileSystem = new MockFileSystem();
+            fileSystem.AddFile(@"X:\SimpEn.csv",
+"""
+KeyValue,EngValue,CnValue
+KeyValue2,EngValue2,CnValue2
+""");
+
+
+            List<CsLocalizationEntry> actual = Program.ParseSimpFile(fileSystem, @"X:\SimpEn.csv");
+
+
+            List<CsLocalizationEntry> expected = new List<CsLocalizationEntry>()
+            {
+                new CsLocalizationEntry("KeyValue", "EngValue", "CnValue"),
+                new CsLocalizationEntry("KeyValue2", "EngValue2", "CnValue2"),
+            };
+
+            Assert.Equivalent(expected, actual);
+        }
+
+        [Fact]
+        public void SimpEn_Trim_TrimsSpaces()
+        {
+            MockFileSystem fileSystem = new MockFileSystem();
+            fileSystem.AddFile(@"X:\SimpEn.csv",
+"""
+KeyValue , EngValue , CnValue 
+""");
+
+
+            List<CsLocalizationEntry> actual = Program.ParseSimpFile(fileSystem, @"X:\SimpEn.csv");
+
+
+            List<CsLocalizationEntry> expected = new List<CsLocalizationEntry>()
+            {
+                new CsLocalizationEntry("KeyValue ", "EngValue", "CnValue"),
+            };
+
+            Assert.Equivalent(expected, actual);
+        }
+
+        [Fact]
+        public void SimpEn_NoFile_ReturnsEmptyList()
+        {
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            List<CsLocalizationEntry> actual = Program.ParseSimpFile(fileSystem, @"X:\SimpEn.csv");
+
+
+            List<CsLocalizationEntry> expected = new List<CsLocalizationEntry>();
+
+            Assert.Equivalent(expected, actual);
+
         }
     }
 }
